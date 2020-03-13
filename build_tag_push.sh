fa11459958ee00c15b752c8b0cae8a6d043613aa
Jay Newstrom <jaynewstrom@gmail.com>
2020-03-13 15:36:12 -0500

    Build/Push from github actions, rather than locally. (#6)
    Build/Push from github actions, rather than locally.

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -2,27 +2,26 @@
 
 set -o errexit
 
-die () {
-    echo >&2 "$@"
-    exit 1
-}
+PUSH=$1
 
-[[ "$#" -eq 2 ]] || die "2 arguments required, $# provided. 1st argument should be gradle version. 2nd argument should be android gradle plugin version."
-
-GRADLE_VERSION=$1
-AGP_VERSION=$2
+source versions.sh
 
 ./gradlew wrapper --gradle-version=${GRADLE_VERSION} # First run changes `gradle/wrapper/gradle-wrapper.properties`.
 ./gradlew wrapper --gradle-version=${GRADLE_VERSION} # Second run regenerates the wrapper.
 
-gsed -i "/androidGradlePluginVersion=/ s/=.*/=${AGP_VERSION}/" gradle.properties
+sed -i "/androidGradlePluginVersion=/ s/=.*/=${AGP_VERSION}/" gradle.properties
 
 VERSION_TAG="gradle-${GRADLE_VERSION}_agp-${AGP_VERSION}"
+
 # Docker.
 docker build -t alpine-android-gradle .
 docker tag alpine-android-gradle:latest jaynewstrom/alpine-gradle-android:${VERSION_TAG}
-docker push jaynewstrom/alpine-gradle-android:${VERSION_TAG}
+if [[ $PUSH == 'true' ]]; then
+  docker push jaynewstrom/alpine-gradle-android:${VERSION_TAG}
+fi
 
 docker build -t remote-build --build-arg CONTAINER_TAG=${VERSION_TAG} -f remoteBuild/Dockerfile remoteBuild/.
 docker tag remote-build:latest jaynewstrom/alpine-gradle-android-remote-build:${VERSION_TAG}
-docker push jaynewstrom/alpine-gradle-android-remote-build:${VERSION_TAG}
+if [[ $PUSH == 'true' ]]; then
+  docker push jaynewstrom/alpine-gradle-android-remote-build:${VERSION_TAG}
+fi
---
+++ versions.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export GRADLE_VERSION=6.2.2
+export AGP_VERSION=3.6.1
